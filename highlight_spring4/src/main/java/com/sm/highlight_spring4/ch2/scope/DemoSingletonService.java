14d85ad7e708bf26ebceccdd010563ff8323a4d7
shenmiao <shenmiao@zjaisino.com>
2017-07-23 14:00:59 +0800

    清空

--- highlight_spring4/src/main/java/com/sm/highlight_spring4/ch2/scope/DemoSingletonService.java
+++
@@ -1,9 +0,0 @@
-package com.sm.highlight_spring4.ch2.scope;
-
-import org.springframework.stereotype.Service;
-
-//默认为Singleton, 相当于@Scope("singleton")
-@Service
-public class DemoSingletonService {
-
-}
--- highlight_spring4/src/main/java/com/sm/highlight_spring4/ch3/taskscheduler/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sm.highlight_spring4.ch3.taskscheduler;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class Main {
-	public static void main(String[] args) {
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TaskSchedulerConfig.class);
-	}
-}
